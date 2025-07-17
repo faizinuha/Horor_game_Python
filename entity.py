cbb1e5e486428559ee0787230d9e45bf09e46f51
Geo <70107300+faizinuha@users.noreply.github.com>
2025-07-17 15:35:14 +0000

    P

--- entity.py
+++ entity.py
@@ -22,3 +22,26 @@ class Entity(pygame.sprite.Sprite):
             self.rect.y += self.speed * dy / dist
 
 
+class Ghost:
+    def __init__(self):
+        self.image = pygame.Surface((40, 60))
+        self.image.fill((255, 0, 0))  # warna merah untuk si hantu
+        self.rect = self.image.get_rect()
+        self.rect.x = 400
+        self.rect.y = 100
+        self.direction = 1
+
+    def update(self, player_rect):
+        # Hantu ngikutin pemain secara sederhana
+        if player_rect.x < self.rect.x:
+            self.rect.x -= 1
+        elif player_rect.x > self.rect.x:
+            self.rect.x += 1
+
+        if player_rect.y < self.rect.y:
+            self.rect.y -= 1
+        elif player_rect.y > self.rect.y:
+            self.rect.y += 1
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
